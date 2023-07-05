098cc8c751ca92b2b7ed9b77eba1d7c5ab7ba3ee
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-07-05 13:25:58 +0200

    read_aruco_markers.py now draws on the image the boundaries and the ID's of the aruco markers

--- read_aruco_markers.py
+++ read_aruco_markers.py
@@ -6,38 +6,29 @@ import time
 def findArucoMarkers(aImage, aDict):
     myImageGrayScaled = cv.cvtColor(aImage, cv.COLOR_BGR2GRAY)
     arucoParameters = aruco.DetectorParameters_create()
-    boundaryBoxes, ID, rejectedImages = aruco.detectMarkers(myImageGrayScaled, aDict, parameters=arucoParameters)
-    print(ID)
+    boundaryBoxes, myID, rejectedImages = aruco.detectMarkers(myImageGrayScaled, aDict, parameters=arucoParameters)
+    print(myID)
+    
+    aruco.drawDetectedMarkers(aImage, boundaryBoxes, myID)
 
 def playVideo():
     myVid = cv.VideoCapture(0)
 
     while 0xFF != ord(' '):
         captureSuccessfull, myFrame = myVid.read()
-        cv.imshow("My Video", myFrame)
+        
         findArucoMarkers(myFrame, aruco.Dictionary_get(aruco.DICT_4X4_50) )
+        cv.imshow("My Video", myFrame)
         cv.waitKey(50)
         
         if cv.waitKey(1) & 0xFF == ord(' '):
             break    
 
-
-
-
 playVideo()
 
 
 
-
-
-
-
-
-
-
-
-
-print("\n\n\nleroleroleroleroleroleroleroleroleroleorlero\n\n\n")
+print("\n\n\nExecution Finished\n\n\n")
 
 
 
